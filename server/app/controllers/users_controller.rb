7280c37b32fadaefac9bf583982f212d1a2d0b8f
kkoomin <minha.koo.9@gmail.com>
2019-03-26 17:25:23 +0000

    password -> password_digest

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     private
 
     def user_params 
-        params.require(:user).permit(:email, :password, :nickname)
+        params.require(:user).permit(:email, :password, :password_confirmation, :nickname)
     end
     
 end
---
+++ server/db/migrate/20190326172000_change_column_password.rb
@@ -0,0 +1,6 @@
+class ChangeColumnPassword < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :password
+    add_column :users, :password_digest, :string
+  end
+end
