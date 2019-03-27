d1fe559befff89c56a1db903908c6edcde3d64c0
kkoomin <minha.koo.9@gmail.com>
2019-03-27 15:06:38 +0000

    petit change

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::API
 
     def decoded_token
         if auth_header
-            token = auth_header.split(' ')[1]
+            token = auth_header
             begin
                 JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
             rescue  JWT::DecodeError
