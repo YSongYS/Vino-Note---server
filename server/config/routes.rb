37e092a19e2d7d3e89455ecd33fa9a8d6b72eb22
YSongYS <songyishu.yang@gmail.com>
2019-03-27 10:55:34 +0000

    small changes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :wines
   resources :users
+  resources :logs
+  resources :smells
+
+  #delete as needed
+  resources :looks
+  resources :tastes
+
 end
